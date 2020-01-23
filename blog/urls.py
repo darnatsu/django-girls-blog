301d89689539ee851e1ed4e021849e2dbd5372fc
Darryl <dar.diapolet@gmail.com>
2020-01-23 21:58:17 +0800

    Partial class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from blog.views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail' ),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:post_id>/', PostDetailView.as_view(), name='post_detail' ),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
+    path('post/edit/<int:post_id>/', views.post_edit, name='post_edit'),
 ]
