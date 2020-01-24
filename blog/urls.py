e9f9367411a00ee4242c4dba78cf11fd16f35a15
Darryl <dar.diapolet@gmail.com>
2020-01-24 16:00:27 +0800

    Fixed Class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from blog.views import PostListView, PostDetailView, PostCreateView
+from blog.views import PostListView, PostDetailView, PostCreateView, PostEditView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:post_id>/', PostDetailView.as_view(), name='post_detail' ),
     path('post/new/', PostCreateView.as_view(), name='post_new'),
-    path('post/edit/<int:post_id>/', views.post_edit, name='post_edit'),
+    path('post/edit/<int:post_id>/', PostEditView.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, Http404
 from .models import Post
 from django.utils import timezone
 from django.views.generic import TemplateView
@@ -9,29 +9,38 @@ from .forms import PostForm
 
 class PostListView(TemplateView):
     
-    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     template_name = 'blog/post_list.html'
-    context = {'post' : post}
-
+    
     def get(self, *args, **kwargs):
-        return render(self.request, self.template_name, self.context)
+        posts = Post.objects.all()
+        context = {
+            'posts':posts
+            }
+
+        return render(self.request, self.template_name, context)
 
 class PostDetailView(TemplateView):
 
     template_name = 'blog/post_detail.html'
-    context = {}
 
     def get(self, *args, **kwargs):
-        return render(self.request, self.template_name, self.context)
 
-    
+        post_id = kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        context = {
+            'post' : post,
+        }
+
+        return render(self.request, self.template_name, context)
+
 class PostCreateView(TemplateView):
 
     template_name = 'blog/post_edit.html'
-    context = {}
 
     def get(self, *args, **kwargs):
-        return render(self.request, self.template_name, self.context)
+        form = PostForm()
+        context = { 'form' : form }
+        return render(self.request, self.template_name, context)
 
     def post(self, *args, **kwargs):
 
@@ -39,26 +48,48 @@ class PostCreateView(TemplateView):
 
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = self.request.user
             post.published_date = timezone.now()
+
             post.save()
-            return redirect('post_detail', pk=post.pk)
+
+            return redirect('post_detail', post_id=post.pk)
 
         else:
+
             form = PostForm()
 
         return render(self.request, self.template_name, self.context)
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+
+class PostEditView(TemplateView):
+    
+    template_name = 'blog/post_edit.html'
+
+    def get(self, *args, **kwargs):
+
+        post_id = kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        form = PostForm(instance=post)
+
+        context = {
+            'form' : form
+        }
+
+        return render(self.request, self.template_name, context)
+
+    def post(self, *args, **kwargs):
+        
+        post_id = kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+
+        form = PostForm(self.request.POST, instance=post)
+
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = self.request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('post_detail', post_id=post.pk)
+        
+        return render(self.request, self.template_name, self.context)
